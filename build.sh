ea0b091a4d59f4433674ac905c9e2b50c290ad07
Kuugo <kuugo@kuugo.top>
2026-07-25 20:53:59 +0800

    修复编译脚本错误，添加USER_NS支持

--- build.sh
+++ build.sh
@@ -191,6 +191,8 @@ make $MAKE_ARGS "${TARGET_DEVICE}_defconfig"
 if $USE_KSU; then
     color_echo "$green" "启用 KernelSU..."
     ./scripts/config --file "$BUILD_DIR/.config" \
+        -e DEBUG_KERNEL \
+        -e KALLSYMS_ALL \
         -e KSU \
         -e KSU_SUSFS
 
